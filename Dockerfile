351d752e82abf86a5ad96961d84cf196b99aa0c5
PavelSergeevichYurevich <Pasha2028@icloud.com>
2024-09-27 17:15:08 +0700

    .

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY . .
-# CMD ["gunicorn", "-k", "uvicorn.workers.UvicornWorker", "--bind", "0.0.0.0:8000", "--access-logfile", "-", "--error-logfile", "-", "main:app"]
-CMD ["PYTHON", "main.py"]
+CMD ["gunicorn", "-k", "uvicorn.workers.UvicornWorker", "--bind", "0.0.0.0:8000", "--access-logfile", "-", "--error-logfile", "-", "main:app"]
+# CMD ["PYTHON", "main.py"]
--- main.py
+++ main.py
@@ -12,12 +12,12 @@ app.include_router(start.start_router)
 app.include_router(user.user_router)
 app.include_router(test.test_router)
 app.mount("/static", StaticFiles(directory=Path(__file__).parent.absolute() / "static"), name="static")
-# Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 
-HOST = '0.0.0.0'
+""" HOST = '0.0.0.0'
 if __name__ == '__main__':
     Base.metadata.create_all(bind=engine)
     print('Starting server')
     uvicorn.run('main:app', port=8000, host=HOST, reload=True)
-    print('Server stopped')
+    print('Server stopped') """
